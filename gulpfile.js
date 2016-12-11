232f70ad8e7627d7250dd49c55a8d066ef411aa1
Ed Paradis <legomaniac@gmail.com>
2016-12-10 17:46:05 -0800

    project setup... took forever >:0

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 var gulp = require('gulp');
-var deploy = require('gulp-gh-pages')
+var deploy = require('gulp-gh-pages');
+var ts = require('gulp-typescript');
+var mocha = require('gulp-mocha');
 
 gulp.task('deploy', function() {
-  return gulp.src("public/**/*")
+  return gulp.src( ['public/**/*', 'build/app.js'])
     .pipe(deploy());
+});
+
+var tsProject = ts.createProject('./tsconfig.json');
+gulp.task('test', function() {
+  gulp.src( ['tests/**/*.ts'], { base: '.' })
+    .pipe(tsProject());
+    
+  return gulp.src( [ 'tests/**/*.js'] )
+    .pipe(mocha({
+        reporter: 'progress'
+    }));
 });
 
 
