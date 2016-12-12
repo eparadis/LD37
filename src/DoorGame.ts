f6da51a9b820441b61b17a47270936238d53aa6f
Ed Paradis <legomaniac@gmail.com>
2016-12-11 18:36:02 -0800

    bleargh

--- src/DoorGame.ts
+++ src/DoorGame.ts
@@ -4,12 +4,14 @@ import {Room} from './Room';
 export class DoorGame {
 
     private view : RoomView;
+    private room : Room;
 
     constructor( view : RoomView ) {
         this.view = view;
+        this.room = new Room();
     }
 
     Run() : void {
-        this.view.Draw(new Room());
+        this.view.Draw(this.room);
     }
 }
--- src/RoomView.ts
+++ src/RoomView.ts
@@ -1,17 +1,17 @@
 
-import {Room} from './Room';
+import { Room } from './Room';
 
 export interface RoomView {
-    Draw( room: Room) : void;
+    Draw(room: Room): void;
 }
 
-export class HTMLRoom implements RoomView{
-    private parent : HTMLElement;
+export class HTMLRoom implements RoomView {
+    private parent: HTMLElement;
     constructor(parent: HTMLElement) {
         this.parent = parent;
     }
 
-    Draw(room: Room) : void {
+    Draw(room: Room): void {
         const isOpenable = room.doorOpenable;
         const playerPosition = room.playerPosition;
 
@@ -21,11 +21,11 @@ export class HTMLRoom implements RoomView{
         const playerParent = document.createElement('div');
         this.parent.appendChild(playerParent);
         playerParent.id = 'playerParent';
-        
-        for( var i = 0; i < numDoors; i += 1) {
+
+        for (var i = 0; i < numDoors; i += 1) {
             const playerCell = document.createElement('div');
             playerCell.className = "playerCell";
-            if( i == playerPosition) {
+            if (i == playerPosition) {
                 playerCell.innerText = "P";
             }
             playerParent.appendChild(playerCell);
@@ -35,16 +35,53 @@ export class HTMLRoom implements RoomView{
         const doorParent = document.createElement('div');
         this.parent.appendChild(doorParent);
         doorParent.id = 'doorParent';
-        
-        for(var i = 0; i < numDoors; i += 1) {
-            const btn = document.createElement('button');
-            btn.innerText = i.toString() + (isOpenable[i] ? " open": " locked");
-            btn.setAttribute('doorNumber', i.toString());
-            if(!isOpenable[i]) {
-                btn.classList.add('disabled');
+
+        for (var i = 0; i < numDoors; i += 1) {
+
+            let name = i.toString();
+            if( i == 0) { 
+                name = "entrance";
             }
-            btn.onclick = function(ev) { room.openDoor( this.getAttribute('doorNumber')); };
-            doorParent.appendChild(btn);
+            if( i == numDoors - 1) {
+                name = "exit";
+            }
+            const door = new Door(doorParent, name);
+            if(isOpenable[i]) {
+                door.unlock();
+            }
         }
+    }
+}
+
+class Door {
+    private openable : boolean = false;
+    private btn : HTMLElement;
+    private name: string;
+
+    constructor(parent: HTMLElement, name: string) {
+        this.name = name;
+        this.btn = document.createElement('button');
+        this.btn.setAttribute('doorNumber', this.name );
+        this.btn.onclick = function (ev) { console.log(name); };
+        this.setAppearance();
+        parent.appendChild(this.btn);
+    }
+
+    unlock() : void {
+        this.openable = true;
+        this.setAppearance();
+    }
+
+    private setAppearance() {
+        this.btn.innerText = this.name + (this.isOpenable() ? " open" : " locked");
+        
+        this.btn.classList.remove('disabled');
+        if (!this.isOpenable()) {
+            this.btn.classList.add('disabled');
+        }
+    }
+
+    private isOpenable() : boolean {
+        return this.openable;
     }
 }
